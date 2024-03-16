361cbb72d30b20c7cacd6b6f997d46000d192f08
Hammad wasi <160735009+H-Was1@users.noreply.github.com>
2024-03-16 21:44:28 +0500

    added puppeteer lambda

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
       "puppeteer-extra-plugin-adblocker",
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Only run the ignore-loader in the server-side webpack configuration
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.map$/,
+        use: {
+          loader: 'ignore-loader'
+        }
+      });
+    }
+
+    return config;
+  }
 };
 
 export default nextConfig;
